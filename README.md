41477229a64ac69201f3b135f465c2af22deda6a
StevenD <dorvilsteven@gmail.com>
2022-01-20 20:19:41 -0500

    create README, add content to movie page and reaction card, current not dynamic

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# shmovies-app
-browse and rent movies
+# SHMOVIES
+
+Created by [Steven Dorvil](https://www.github.com/dorvilsteven), [Mi](https://www.github.com/nguyenmi18), [Hiroshi](https://www.github.com/Hiroshi777300ER)
+
+## Table of Contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Questions](#Questions)
+
+## Description
+
+Visit our web page and create a movie post and also post reactions (coming soon).
+
+## Installation
+
+[SHMOVIES](https://shmovies.herokuapp.com/)
+
+simply visit the website [SHMOVIES](https://shmovies.herokuapp.com/), or visit the git repository and clone it so that you can run on your local machine.
+
+## Usage
+
+Users can login and create movie post as well as create movie reactions
+
+## License
+
+[License](https://opensource.org/licenses/MIT)
+
+## Questions
+
+[Github](https://www.github.com/dorvilsteven)
+
+If by any chance you want to reach out directly to me with questions about SHMOVIES, here's my email, dorvilsteven@gmail.com. Feel free to send any questions, criticisms my way, please allow at least 24 hours before a response.
--- client/src/pages/Movie.js
+++ client/src/pages/Movie.js
@@ -1,13 +1,92 @@
 import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_MOVIE } from '../utils/queries';
+import MovieCard from "../components/MovieCard/MovieCard";
+import ReactionCard from "../components/ReactionCard/ReactionCard";
+import { Form, Button, Alert } from "react-bootstrap";
 
-const Movie = () => {
-
+const Movie = (props) => {
+    const {
+        movieId
+    } = props
+    const { loading, data } = useQuery(QUERY_MOVIE);
     return (
-        <>
-            <h1>See Movie Here!</h1>
-            <div>Comment Section</div>
-            {/* {Reactions} */}
-        </>
+        <div 
+            className="single-movie-container"
+            style={{
+                display: "flex",
+                flexWrap: "nowrap",
+                justifyContent: "space-around"
+            }}>
+            <div
+                style={{
+                    border: "1px solid #292B2C"
+                }}
+            >
+                    See Movie Here!
+                    <MovieCard />
+            </div>
+            <div 
+                className="comment-section"
+            >
+                <h3>Reactions</h3>
+                <Form
+                    style={{
+                    border: "1px solid #292B2C",
+                    padding: "1em",
+                    width: "50%",
+                    margin: "auto",
+                    }}
+                    // onSubmit={handleFormSubmit}
+                >
+                    
+                    Mov
+                    {/* title input */}
+                    <Form.Group>
+                    <Form.Label htmlFor="title">Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Title"
+                        name="title"
+                        required
+                    />
+                    </Form.Group>
+                    {/* director input */}
+                    <Form.Group>
+                    <Form.Label htmlFor="email">Director</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Director"
+                        name="director"
+                        required
+                    />
+                    </Form.Group>
+                    {/* category input */}
+                    <Form.Group>
+                    <Form.Label htmlFor="password">Category</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Category"
+                        name="category"
+                        required
+                    />
+                    </Form.Group>
+                    <Button
+                        type="submit"
+                        variant="success"
+                        style={{
+                        margin: "auto",
+                        }}
+                    >
+                        Post Movie
+                    </Button>
+                </Form>
+                {/* {Reactions} */}
+                <ReactionCard />
+                <ReactionCard />
+                <ReactionCard />
+            </div>
+        </div>
     );
 }
 
